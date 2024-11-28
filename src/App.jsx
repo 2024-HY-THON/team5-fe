8db9343756bcbeb25ac63bc0486d192ea589645b
SeHyun <rla6170@naver.com>
2024-11-28 15:49:15 +0900

    feat: community - real time popular selected

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import * as S from './styles/Common/common.style';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Community from './pages/Community';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/community" element={<Community />} />
           </Routes>
         </S.CommonWrapper>
       </div>
---
+++ src/pages/Community/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as S from '../../styles/Community/community.style';
+
+const Community = () => {
+  return (
+    <S.CommunityContainer>
+      <S.CommunityContents>
+        <S.CommunityCategoryBox>
+          <S.CommunityCategory selected>실시간 별록</S.CommunityCategory>
+          <S.CommunityCategory>친구 별록</S.CommunityCategory>
+        </S.CommunityCategoryBox>
+        <S.CommunityContentHeader>실시간 인기 별록</S.CommunityContentHeader>
+        <S.CommunityContent style={{ backgroundColor: '#70C1FF' }}>
+          <img src="" alt="" />
+          <div>
+            <S.CommunityContentTitle>#제목</S.CommunityContentTitle>
+            <S.CommunityContentText>내용내용내용</S.CommunityContentText>
+          </div>
+          <button>
+            <img src="" alt="" />
+            <span>12개</span>
+          </button>
+        </S.CommunityContent>
+        <S.CommunityContent
+          style={{ backgroundColor: '#65EE83' }}
+        ></S.CommunityContent>
+      </S.CommunityContents>
+      <S.CommunityContentLists>
+        <S.CommunityContentList>
+          <span>#산책</span>
+          <div>
+            내용내용내용
+            <br />
+            내용내용
+            <br />
+            내용내용내용
+          </div>
+        </S.CommunityContentList>
+      </S.CommunityContentLists>
+    </S.CommunityContainer>
+  );
+};
+
+export default Community;
