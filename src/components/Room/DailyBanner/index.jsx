2798204a81d75dfab797a2bdd035fb08d1fff8e8
glosona <hayoung.s@nglinnovation.net>
2024-11-30 01:51:01 +0900

    hotfix: Fix name

--- src/components/Room/DailyBanner/index.jsx
+++ src/components/Room/DailyBanner/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { HomeBanner, BannerContent } from '../../../styles/Home/home.style';
-import { STAR_CHARACTER, ARROW_RIGHT } from '../../../constants/Common/icon';
+import { STAR_YELLOW, ARROW_RIGHT } from '../../../constants/Common/icon';
 
 const DailyBanner = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col gap-y-[15px]">
       <h3 className="pl-[10px]">가장 많은 도장을 받은 별록</h3>
-      <HomeBanner>
+      <HomeBanner onClick={() => navigate('/room/detail')}>
         <BannerContent>
           <div className="flex gap-x-[10px] justify-center items-center">
             <img
-              src={STAR_CHARACTER}
+              src={STAR_YELLOW}
               alt="logo"
               style={{ width: '47px', height: '44px' }}
             />
--- src/components/Room/MyReward/index.jsx
+++ src/components/Room/MyReward/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import RewardWall from './RewardWall';
-import { STAR_CHARACTER } from '../../../constants/Common/icon';
+import { STAR_YELLOW } from '../../../constants/Common/icon';
 
 const MyReward = () => {
   return (
@@ -8,7 +8,7 @@ const MyReward = () => {
       <h3 className="pl-[10px]">내가 받은 상장</h3>
       <RewardWall />
       <div className="w-[193px] h-[178px] absolute left-1/2 bottom-[-40px] transform -translate-x-1/2">
-        <img src={STAR_CHARACTER} alt="logo" className="w-full h-full" />
+        <img src={STAR_YELLOW} alt="logo" className="w-full h-full" />
       </div>
     </div>
   );
