3e4128bdef0a9d9a40231feb20528e2b0beb1fcd
glosona <hayoung.s@nglinnovation.net>
2024-11-30 16:01:13 +0900

    hotfix: Add reward wall

--- src/components/Room/RoomMain/RewardWallSection/RewardWall/index.jsx
+++ src/components/Room/RoomMain/RewardWallSection/RewardWall/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const RewardWall = () => {
-  const dummyData = [...Array(36).keys()];
+  // const dummyData = [...Array(36).keys()];
   return (
-    <div className="self-center grid grid-cols-9 grid-rows-4 gap-[8px] w-[345px]">
-      {dummyData.map((_, index) => (
-        <div className="w-[31px] h-[44px] bg-[#d9d9d9]" key={index} />
-      ))}
-    </div>
+    <img src="/assets/Common/reward_wall.svg" alt="reward_wall" />
+    // <div className="self-center grid grid-cols-9 grid-rows-4 gap-[8px] w-[345px]">
+    //   <img src="/assets/Common/reward_wall.svg" alt="reward_wall" />
+    //   {dummyData.map((_, index) => (
+    //     <div className="w-[31px] h-[44px] bg-[#d9d9d9]" key={index} />
+    //   ))}
+    // </div>
   );
 };
 
